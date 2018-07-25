fefc575d77882001a6eedcabe702c4ee7c4ca320
peixoto <peixoto@textecnologia.com.br>
2018-07-25 17:12:38 -0300

    primeiro test

---
+++ test.js
@@ -0,0 +1,19 @@
+const request = require('supertest')
+var chai = require('chai')
+var expect = chai.expect
+var assert = chai.assert
+
+describe('GET returns a valid json', function(){
+    it('teste 1', function(done) {
+        request('https://jsonplaceholder.typicode.com')
+            .get('/posts/1')
+            .end(function(err, res) {
+                assert.equal(res.body.id, 2)
+                expect(res.statusCode).to.equal(200)
+                expect(res.body.title).to.be.an('string')
+                expect(res.body.id).to.be.a('number')
+                expect(res.body.id).to.equal(1)
+                done()
+            })
+    }).timeout(5000)
+})
